2dcad47bb921170070f8d4bae1ce07244547db5c
James Couball <jcouball@yahoo.com>
2025-05-22 09:35:56 -0700

    feat!: remove Result#stdout and Result#stderr
    Do not automatically capture stdout and stderr and return it in the Result object.
    
    The way it was implemented, stdout and stderr were only captured if wrapped in a
    MonitoredPipe. It also meant that every MonitoredPipe output destination was captured
    even if it was not from stdout or stderr.
    
    The new method will be added to the API `ProcessExecuter.run_with_capture` that
    captures ONLY stdout and stderr and returns a `ResultWithCapture` which derives from
    `Result` and has `#stdout` and `#stderr` methods.
    
    BREAKING CHANGE: Users depending on `Result#stdout` or `Result#stderr` will either
    have to capture this output manually themselves or change from `spawn_and_wait`/`run`
    to `run_with_capture`.

--- lib/process_executer/destination_base.rb
+++ lib/process_executer/destination_base.rb
@@ -22,27 +22,13 @@ module ProcessExecuter
     # @return [Object] the destination object
     attr_reader :destination
 
-    # The data written to the destination
-    #
-    # @return [Array<String>] the data written to the destination
-    attr_reader :data_written
-
-    # The data written to the destination as a single string
-    # @return [String]
-    def string
-      data_written.join
-    end
-
     # Writes data to the destination
     #
     # This is an abstract method that must be implemented by subclasses.
     #
     # @param data [String] the data to write
     # @return [void]
-    # @raise [NotImplementedError] if the subclass doesn't implement this method
-    def write(data)
-      @data_written << data
-    end
+    def write(data); end
 
     # Closes the destination if necessary
     #
